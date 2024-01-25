fa5bae698072b6be3366fc18e030dd87ad3b09d1
Yathil <oliver.a.perez@hotmail.com>
2024-01-25 17:39:33 +0100

    metodo calculadora

--- src/App.java
+++ src/App.java
@@ -1,5 +1,30 @@
 public class App {
+
+    private int calculadora(int num1, int num2, String operacion) {
+        int resultado = 0;
+        switch (operacion) {
+            case "+":
+                resultado = num1 + num2;
+                break;
+            case "-":
+                resultado = num1 - num2;
+                break;
+            case "*":
+                resultado = num1 * num2;
+                break;
+            case "/":
+                resultado = num1 / num2;
+                break;
+            default:
+                System.out.println("Operacion invalida");
+                break;
+        }
+        return resultado;
+    }
+
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        App app = new App();
+        int resultado = app.calculadora(10, 5, "+");
+        System.out.println(resultado);
     }
 }
